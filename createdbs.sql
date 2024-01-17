a3b1b3f1c903d1f274e881a1d0f92f80325bc45e
dimacoin <dima@unic.ac.cy>
2024-01-17 12:30:07 +0000

    ccc

--- createdbs.sql
+++ createdbs.sql
@@ -1,7 +1,5 @@
-drop database  accelerate;
-drop database  cerbos;
-create database if not exists accelerate;
 create database if not exists cerbos;
+drop database  cerbos;
+create database cerbos;
 create user if not exists 'accelerate'@'%' identified WITH mysql_native_password BY 'accelerate';
-grant all privileges on accelerate.* to 'accelerate'@'%';
 grant all privileges on cerbos.* to 'accelerate'@'%';
