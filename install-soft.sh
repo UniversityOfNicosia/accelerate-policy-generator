3a5069e9565128e8442b8b6b83c8f2d633745267
HlexNC <hlexhelftd@gmail.com>
2024-01-18 11:49:33 +0000

    Update install-soft.sh script with logging statements

--- install-soft.sh
+++ install-soft.sh
@@ -1,13 +1,13 @@
-echo "updating repositories..."
+echo "updating repositories..." | tee -a install.log
 sudo apt-get update
 #sudo apt-get upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
-echo "installing mysql"
+echo "installing mysql" | tee -a install.log
 sudo apt-get install -y mysql-server
-echo "starting service ..."
+echo "starting service ..." | tee -a install.log
 sudo cp ./mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
-echo "creating the database ..."
+echo "creating the database ..." | tee -a install.log
 sudo mysql -u root < ./createdbs.sql
 sudo mysql -u root < ./cerbos.sql
-echo "installing cerbos ..."
+echo "installing cerbos ..." | tee -a install.log
 docker compose up
