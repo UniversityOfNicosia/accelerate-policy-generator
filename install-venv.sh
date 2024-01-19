d31edf68415a5ef49c40f222dc8d73c12023c1d4
HlexNC <hlexhelftd@gmail.com>
2024-01-19 14:51:49 +0000

    Update virtual environment setup

--- install-venv.sh
+++ install-venv.sh
@@ -1,14 +1,18 @@
 # Get the Python interpreter path from VS Code settings
 echo "Getting Python interpreter path from VS Code settings..." | tee -a install.log
-PYTHON_PATH=$(jq -r '.["python.pythonPath"]' .vscode/settings.json)
+PYTHON_PATH=$(which python3)
 
 # Create a virtual environment using the specified Python interpreter |
 echo "Creating a virtual environment using the specified Python interpreter..." | tee -a install.log
-$PYTHON_PATH -m venv venv
+$PYTHON_PATH -m venv .venv
+
+# Create a .gitignore file in the .venv directory
+echo "Creating a .gitignore file in the .venv directory..." | tee -a install.log
+echo "*" > .venv/.gitignore
 
 # Activate the virtual environment
 echo "Activating the virtual environment..." | tee -a install.log
-source venv/bin/activate
+source .venv/bin/activate
 
 # Install libraries from requirements.txt
 echo "Installing libraries from requirements.txt..." | tee -a install.log
